88a03f5006148fc21931c07d88501f9d41dbbc52
Kennedy <zxc1258741@gmail.com>
2022-04-07 21:07:01 +0800

    Initial commit

---
+++ cmd/web/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Kennedy/bookings/pkg/config"
+	"github.com/Kennedy/bookings/pkg/handlers"
+	"github.com/Kennedy/bookings/pkg/render"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+)
+
+const portNum = ":8080"
+
+var app config.AppConfig
+var session *scs.SessionManager
+
+func main() {
+
+	// change this to true when in production
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+	app.Session = session
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandler(repo)
+
+	render.NewTemplates(&app)
+
+	fmt.Println("Starting application on port", portNum)
+
+	server := &http.Server{
+		Addr:    portNum,
+		Handler: routes(&app),
+	}
+	err = server.ListenAndServe()
+	log.Fatal(err)
+}
